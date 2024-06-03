4b292f1a4b4d3ab969c018a6e9fcd727bc643d9d
Joseph Weinkam <joseph.m.weinkam@gmail.com>
2024-06-03 11:31:37 -0700

    minor test runner script changes

--- load-test/src/test-runner.ps1
+++ load-test/src/test-runner.ps1
@@ -1,4 +1,8 @@
+# Warm up before running heavier load test
+# npm run stepTest -- -e VUS=5 -e DUR=2m -e WAIT=false
+
 # Step Test - Increase vus each run
+# Scenario 1: Increase by 10 each time
 # npm run stepTest -- -e VUS=10 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=20 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=30 -e DUR=10m -e WAIT=false
@@ -6,15 +10,13 @@
 # npm run stepTest -- -e VUS=50 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=60 -e DUR=10m -e WAIT=false
 
-# Warm up before running heavier load test
-# npm run stepTest -- -e VUS=5 -e DUR=2m -e WAIT=false
-
+# Scenario 2: Increase by 5 each time
 # npm run stepTest -- -e VUS=15 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=20 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=25 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=30 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=35 -e DUR=10m -e WAIT=false
-npm run stepTest -- -e VUS=40 -e DUR=10m -e WAIT=false
+# npm run stepTest -- -e VUS=40 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=45 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=50 -e DUR=10m -e WAIT=false
 # npm run stepTest -- -e VUS=55 -e DUR=10m -e WAIT=false
@@ -22,3 +24,4 @@ npm run stepTest -- -e VUS=40 -e DUR=10m -e WAIT=false
 
 
 # npm run regNewProfile
+npm run regNewProfile -- -e VUS=20 -e ITERS=1
